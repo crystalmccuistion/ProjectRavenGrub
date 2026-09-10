702ab319e7c586f5ef8c7eb94652e41ec9a347f5
Crystal McCuistion <crystalmccuistion26@gmail.com>
2026-09-10 14:46:38 -0500

    Add empty ItemDefinition C++ class
    ItemDefinition is a C++ class, so this also converts the UE5 project to a C++ project.

---
+++ Source/ProjectRavenGrub/Items/ItemDefinition.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ItemDefinition.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRAVENGRUB_API UItemDefinition : public UObject
+{
+	GENERATED_BODY()
+	
+};
