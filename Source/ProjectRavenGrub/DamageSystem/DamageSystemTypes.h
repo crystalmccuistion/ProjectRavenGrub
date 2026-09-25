dd0d664ced2fa6ffa314409a472df0fda54a96f5
Crystal McCuistion <crystalmccuistion26@gmail.com>
2026-09-24 23:01:03 -0500

    Update character system.
    Add early C++ classes for character damage system. Add base character blueprint. Change player pawn blueprint's parent class to base character blueprint. Create enemy character blueprint.

---
+++ Source/ProjectRavenGrub/DamageSystem/DamageSystemTypes.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DamageSystemTypes.generated.h"
+
+UENUM(BlueprintType)
+enum class EDamageType : uint8
+{
+	None UMETA(DisplayName = "None"),
+	Combatant UMETA(DisplayName = "Combatant"),
+	Environmental UMETA(DisplayName = "Environmental"),
+	StatusEffect UMETA(DisplayName = "Status Effect"),
+};
+
+USTRUCT(BlueprintType)
+struct FDamageInfo
+{
+	GENERATED_BODY();
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	float DamageAmount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	float StaggerAmount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	AActor* DamageCauser;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	bool ShouldDamageInvincible = false;
+};
---
+++ Source/ProjectRavenGrub/DamageSystem/DamageableInterface.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DamageSystemTypes.h"
+#include "UObject/Interface.h"
+#include "DamageableInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UDamageableInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PROJECTRAVENGRUB_API IDamageableInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Damageable Interface")
+	float GetCurrentHealth();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Damageable Interface")
+	float GetMaxHealth();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Damageable Interface")
+	bool GetIsDead();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Damageable Interface")
+	void Heal(float HealAmount, AActor* Healer);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Damageable Interface")
+	bool TakeDamage(FDamageInfo DamageInfo);
+};
